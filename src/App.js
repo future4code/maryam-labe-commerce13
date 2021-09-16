224b39c881f2a500d21b27ef2ffdcf33b4186533
Milena Lara <milenalrf@gmail.com>
2021-09-16 20:29:04 -0300

    Atualizei a lista de produtos com categorias de data e foto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Carrinho from './components/Carrinho/Carrinho';
 import CompraDeProdutos from './components/CompraDeProdutos/CompraDeProdutos';
@@ -10,32 +9,58 @@ class App extends React.Component {
       {
         id: 1,
         titulo: 'Viagem para Vênus',
-        preco: 900000
+        preco: 900000,
+        data: '07/01/2121',
+        foto: ''
       },
       {
         id: 2,
         titulo: 'Viagem para Marte',
-        preco: 1100000
+        preco: 1100000,
+        data: '21/02/2121',
+        foto: ''
       },
       {
         id: 3,
         titulo: 'Viagem Júpiter',
-        preco: 1000000
+        preco: 1000000,
+        data: '06/03/2121',
+        foto: ''
       },
       {
         id: 4,
         titulo: 'Excursão Plêiades',
-        preco: 1300000
+        preco: 1300000,
+        data: '25/04/2121',
+        foto: ''
       },
       {
         id: 5,
         titulo: 'Excursão Andrômeda',
-        preco: 4000000
+        preco: 4000000,
+        data: '30/05/2121',
+        foto: ''
       },
       {
         id: 6,
-        titulo: 'Viagem + show da Lady Gaga no planeta Chromatica',
-        preco: 1000000
+        titulo: 'Viagem + Show da Lady Gaga no planeta Chromatica',
+        preco: 1000000,
+        data: '17/06/2121',
+        foto: ''
+      },
+      {
+        id: 7,
+        titulo: 'Viagem para Lua',
+        preco: 200000,
+        data: '17/07/2121',
+        foto: ''
+      },
+      {
+        id: 8,
+        titulo: 'Viagem para Aldebaran',
+        preco: 1200000,
+        data: '06/08/2121',
+        foto: ''
       },
     ],
     carrinho: []
